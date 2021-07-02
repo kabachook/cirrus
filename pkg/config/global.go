fc295e082171154711e74d9f4922b9b2ec94f74e
Danil <beltyukovdanil@gmail.com>
2021-07-02 17:33:58 +0300

    logger levels

--- pkg/config/global.go
+++ pkg/config/global.go
@@ -1,9 +1,20 @@
 package config
 
-import "go.uber.org/zap"
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
 
 var Logger *zap.Logger
 
 func init() {
-	Logger, _ = zap.NewDevelopment()
+	env := os.Getenv("CIRRUS_ENV")
+	if env != "production" {
+		Logger, _ = zap.NewDevelopment()
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+		Logger, _ = zap.NewProduction()
+	}
 }
